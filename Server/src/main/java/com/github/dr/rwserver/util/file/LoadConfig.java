915145862c026c43cb569752e7a53df8b931a321
Minxyzgo <1225327866@qq.com>
2021-05-19 21:44:44 +0800

    ok

--- Server/src/main/java/com/github/dr/rwserver/util/file/LoadConfig.java
+++ Server/src/main/java/com/github/dr/rwserver/util/file/LoadConfig.java
@@ -6,6 +6,11 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.github.dr.rwserver.struct.OrderedMap;
 import com.github.dr.rwserver.util.log.Log;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,6 +35,13 @@ public final class LoadConfig {
     }
 
     public void reLoadConfig() {
+        if(!fileUtil.exists()) {
+            try {
+                Files.copy(this.getClass().getResourceAsStream("/Config.json"), fileUtil.getFile().toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         JSONObject  jsonObject = JSONObject.parseObject(fileUtil.readFileData(false).toString(), Feature.OrderedField);
         //json对象转Map
         Map<String,Object> map = jsonObject.getInnerMap();

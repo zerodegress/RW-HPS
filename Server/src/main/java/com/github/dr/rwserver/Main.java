e985344a75a9629725d9aeab50720cf68a74af94
deng-rui <deng-rui@foxmail.com>
2021-07-27 10:12:09 +0800

    Old Version Game 1.14

--- Server/src/main/java/com/github/dr/rwserver/Main.java
+++ Server/src/main/java/com/github/dr/rwserver/Main.java
@@ -8,6 +8,7 @@ import com.github.dr.rwserver.core.Initialization;
 import com.github.dr.rwserver.core.thread.Threads;
 import com.github.dr.rwserver.data.global.Data;
 import com.github.dr.rwserver.data.global.NetStaticData;
+import com.github.dr.rwserver.data.plugin.PluginEventManage;
 import com.github.dr.rwserver.data.plugin.PluginManage;
 import com.github.dr.rwserver.dependent.LibraryManager;
 import com.github.dr.rwserver.func.StrCons;
@@ -15,19 +16,18 @@ import com.github.dr.rwserver.game.Event;
 import com.github.dr.rwserver.game.EventType;
 import com.github.dr.rwserver.net.netconnectprotocol.GameVersionPacket;
 import com.github.dr.rwserver.struct.Seq;
-import com.github.dr.rwserver.util.Convert;
 import com.github.dr.rwserver.util.encryption.Autograph;
 import com.github.dr.rwserver.util.encryption.Base64;
 import com.github.dr.rwserver.util.file.FileUtil;
 import com.github.dr.rwserver.util.file.LoadConfig;
 import com.github.dr.rwserver.util.game.CommandHandler;
 import com.github.dr.rwserver.util.game.Events;
+import com.github.dr.rwserver.util.io.IoReadConversion;
 import com.github.dr.rwserver.util.log.Log;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -43,12 +43,22 @@ public class Main {
 	 * 设置多个检查点, 定期检查, 如果发现问题就加密或混淆部分数据
 	 */
 	public static void main(String[] args) {
-		Log.set("ALL");
+		final Initialization initialization = new Initialization();
+
+		Log.set("ERROR");
 		Log.setPrint(true);
 		Logger.getLogger("io.netty").setLevel(Level.OFF);
 
 		System.out.println(Data.localeUtil.getinput("server.login"));
 		Log.clog("Load ing...");
+
+		//byte[] bytes = new Sha().sha256Array("8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92");
+		//String text = new BigInteger(1, bytes).toString(16).toUpperCase(Locale.ROOT);
+		//Log.clog(ExtractUtil.bytesToHex(bytes));
+		//Log.clog(ExtractUtil.bytesToHex(new BigInteger(ExtractUtil.hexToByteArray(text.toLowerCase(Locale.ROOT))).toByteArray()));
+		//Log.clog(new Sha().sha256("123456"));
+		//Log.clog(Arrays.toString(ExtractUtil.hexToByteArray("ff")));
+		//Core.mandatoryExit();
 
 		/* 防止修改签名 */
 		/* CP #1 */
@@ -57,13 +67,16 @@ public class Main {
 			Core.mandatoryExit();
 		}
 
-		Data.core.load();
+		FileUtil.setFilePath((args.length > 0) ? Base64.decodeString(args[0]) : null);
 
-		new Initialization();
+		//Data.core.settings.load();
+		Data.core.load();
 
 		Log.clog(Data.localeUtil.getinput("server.hi"));
 
 		Data.config = new LoadConfig(Data.Plugin_Data_Path,"Config.json");
+
+		initialization.startInit();
 
 		/* 命令加载 */
 		new ServerCommands(Data.SERVERCOMMAND);
@@ -72,11 +85,11 @@ public class Main {
 		Log.clog(Data.localeUtil.getinput("server.load.command"));
 
 		/* Event加载 */
-		new Event();
+		PluginEventManage.add(new Event());
 		Log.clog(Data.localeUtil.getinput("server.load.events"));
 
 		/* 初始化Plugin */
-		PluginManage.init(FileUtil.file(Data.Plugin_Plugins_Path));
+		PluginManage.init(FileUtil.toFolder(Data.Plugin_Plugins_Path));
 		PluginManage.runOnEnable();
 		PluginManage.runRegisterClientCommands(Data.CLIENTCOMMAND);
 		PluginManage.runRegisterServerCommands(Data.SERVERCOMMAND);
@@ -103,31 +116,12 @@ public class Main {
 		Data.SERVERCOMMAND.handleMessage("start",(StrCons) Log::clog);
 	}
 
-	private static void loadCoreJar(String libPath) {
-		LibraryManager lib;
-		if (notIsBlank(libPath)) {
-			lib = new LibraryManager(libPath);
-		} else {
-			lib = new LibraryManager(true,Data.Plugin_Lib_Path);
-		}
-		lib.importLib("io.netty","netty-all","4.1.65.Final");
-		lib.importLib("com.ip2location","ip2location-java","8.5.0");
-		lib.importLib("com.alibaba","fastjson","1.2.58");
-		//lib.importLib("org.quartz-scheduler","quartz","2.3.2");
-
-		//lib.importLib("com.github.oshi","oshi-core","5.5.0");
-		//lib.importLib("net.java.dev.jna","jna","5.7.0");
-		//lib.importLib("org.slf4j","slf4j-api","1.7.30");
-		lib.loadToClassLoader();
-		lib.removeOldLib();
-	}
-
 	@SuppressWarnings("InfiniteLoopStatement")
 	private static void buttonMonitoring() {
-		Scanner scanner = new Scanner(System.in);
+		BufferedReader bufferedReader = IoReadConversion.streamBufferRead(System.in);
 		while (true) {
 			try {
-				String str = scanner.nextLine();
+				String str = bufferedReader.readLine();
 				CommandHandler.CommandResponse response = Data.SERVERCOMMAND.handleMessage(str, (StrCons) Log::clog);
 				if (response != null && response.type != CommandHandler.ResponseType.noCommand) {
 					if (response.type != CommandHandler.ResponseType.valid) {
@@ -143,7 +137,7 @@ public class Main {
 					}
 				}
 			} catch (Exception e) {
-				Log.clog("Error");
+				//Log.clog("Error");
 				//e.printStackTrace();
 			}
 		}
@@ -154,7 +148,7 @@ public class Main {
 		try {
 			DataOutputStream stream = Data.utilData.stream;
 			stream.writeInt(1);
-			Seq<String> list = Convert.castSeq(FileUtil.readFileData(true, new InputStreamReader(Main.class.getResourceAsStream("/unitData"), StandardCharsets.UTF_8)),String.class);
+			Seq<String> list = FileUtil.readFileListString(Objects.requireNonNull(Main.class.getResourceAsStream("/unitData-114")));
 			stream.writeInt(list.size());
 			String[] unitdata;
 			for (String str : list) {

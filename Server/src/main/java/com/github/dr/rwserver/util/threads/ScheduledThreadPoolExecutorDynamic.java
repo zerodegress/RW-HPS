fb0941e5488e48a210fbabc747d168a33197209b
deng-rui <deng-rui@foxmail.com>
2021-07-27 10:23:27 +0800

    Delete dynamic thread pool (bug?)

--- Server/src/main/java/com/github/dr/rwserver/util/threads/ScheduledThreadPoolExecutorDynamic.java
+++ Server/src/main/java/com/github/dr/rwserver/util/threads/ScheduledThreadPoolExecutorDynamic.java
@@ -1,34 +1,52 @@
 package com.github.dr.rwserver.util.threads;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 
+/**
+ * 动态改变线程池的线程数量
+ * @author Dr
+ */
+@Deprecated(forRemoval = true)
 public class ScheduledThreadPoolExecutorDynamic extends ScheduledThreadPoolExecutor {
 
     public ScheduledThreadPoolExecutorDynamic(int corePoolSize) {
         super(corePoolSize);
     }
 
-    public ScheduledThreadPoolExecutorDynamic(int corePoolSize, ThreadFactory threadFactory) {
+    public ScheduledThreadPoolExecutorDynamic(int corePoolSize, @NotNull final ThreadFactory threadFactory) {
         super(corePoolSize,threadFactory);
     }
 
+    @NotNull
     @Override
-    public ScheduledFuture<?> schedule(Runnable command, long end, TimeUnit unit) {
+    public ScheduledFuture<?> schedule(@NotNull final Runnable command, long end, @NotNull final TimeUnit unit) {
         dynamicCorePoolSize(this);
         return super.schedule(command, end, unit);
     }
 
+    @NotNull
     @Override
-    public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit) {
+    public ScheduledFuture<?> scheduleAtFixedRate(@NotNull final Runnable command, long initialDelay, long period, @NotNull final TimeUnit unit) {
         dynamicCorePoolSize(this);
         return super.scheduleAtFixedRate(command, initialDelay, period, unit);
     }
 
-    private static void dynamicCorePoolSize(ScheduledThreadPoolExecutor scheduledThreadPoolExecutor) {
+    public boolean cancelSchedule(@NotNull final ScheduledFuture<?> scheduledFuture) {
+        if (!scheduledFuture.isCancelled()) {
+            scheduledFuture.cancel(true);
+            this.setCorePoolSize(this.getCorePoolSize() - 1);
+            return true;
+        }
+        return false;
+    }
+
+    private static void dynamicCorePoolSize(@NotNull final ScheduledThreadPoolExecutor scheduledThreadPoolExecutor) {
         int activeCount = scheduledThreadPoolExecutor.getActiveCount();
         int corePoolSize = scheduledThreadPoolExecutor.getCorePoolSize();
         if (activeCount == corePoolSize) {
@@ -36,14 +54,5 @@ public class ScheduledThreadPoolExecutorDynamic extends ScheduledThreadPoolExecu
         } else if (activeCount < corePoolSize - 1) {
             scheduledThreadPoolExecutor.setCorePoolSize(corePoolSize - 1);
         }
-    }
-
-    public boolean cancelSchedule(ScheduledFuture<?> scheduledFuture) {
-        if (scheduledFuture != null && !scheduledFuture.isCancelled()) {
-            scheduledFuture.cancel(true);
-            this.setCorePoolSize(this.getCorePoolSize() - 1);
-            return true;
-        }
-        return false;
     }
 }

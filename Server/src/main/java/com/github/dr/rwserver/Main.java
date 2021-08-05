0e40988bb454036ccd4af20cfabe07d77c8656f6
Hxx <aljyzsjy@163.com>
2021-08-06 01:14:32 +0800

    -multiGame-alpha-p

--- Server/src/main/java/com/github/dr/rwserver/Main.java
+++ Server/src/main/java/com/github/dr/rwserver/Main.java
@@ -121,7 +121,7 @@ public class Main {
 		new YouXiBan().registerServerCommands(Data.SERVERCOMMAND);
 		Data.SERVERCOMMAND.handleMessage("start",(StrCons) Log::clog);
 		new UpList().registerServerCommands(Data.SERVERCOMMAND);
-//		Data.SERVERCOMMAND.handleMessage("upserverlist",(StrCons) Log::clog);
+		Data.SERVERCOMMAND.handleMessage("upserverlist",(StrCons) Log::clog);
 		Data.SERVERCOMMAND.handleMessage("timer n",(StrCons) Log::clog);
 	}
 

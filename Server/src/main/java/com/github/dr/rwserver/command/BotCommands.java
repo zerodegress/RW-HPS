fbcbb398222cab7767eb894f005246527e711520
deng-rui <deng-rui@foxmail.com>
2021-08-08 22:27:23 +0800

    Update

--- Server/src/main/java/com/github/dr/rwserver/command/BotCommands.java
+++ Server/src/main/java/com/github/dr/rwserver/command/BotCommands.java
@@ -1,6 +1,6 @@
 package com.github.dr.rwserver.command;
 
-import com.github.dr.rwserver.net.core.AbstractNetConnect;
+import com.github.dr.rwserver.net.core.server.AbstractNetConnect;
 import com.github.dr.rwserver.util.game.CommandHandler;
 
 public class BotCommands {

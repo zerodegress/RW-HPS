8b5b56094b1ff3040cb69b0146574f7534fb948d
deng-rui <dr@der.kim>
2022-06-04 14:28:40 +0800

    fix gradle.kts - jar

--- Server-All/build.gradle.kts
+++ Server-All/build.gradle.kts
@@ -3,6 +3,30 @@ dependencies {
 }
 
 tasks.jar {
+	// Fuck Java 9
+	exclude("**/module-info.class")
+
+	exclude("META-INF/LICENSE.txt")
+	exclude("META-INF/LICENSE")
+	exclude("META-INF/NOTICE.txt")
+	exclude("META-INF/NOTICE")
+
+	// Fuck Netty !!!!!!
+	exclude("META-INF/INDEX.LIST")
+	exclude("META-INF/*.properties")
+	exclude("META-INF/*/*.properties")
+	exclude("META-INF/*/*/*.properties")
+	exclude("META-INF/*/*/*/*.properties")
+	exclude("META-INF/*.xml")
+	exclude("META-INF/*/*.xml")
+	exclude("META-INF/*/*/*.xml")
+	exclude("META-INF/*/*/*/*.xml")
+
+	manifest {
+		attributes(mapOf("Main-Class" to "cn.rwhps.server.Main"))
+		attributes(mapOf("Launcher-Agent-Class" to  "cn.rwhps.server.dependent.LibraryManager"))
+	}
+
 	from(configurations.runtimeClasspath.get().map {
 		if (it.isDirectory) it else zipTree(it)
 	})
--- Server-Core/build.gradle.kts
+++ Server-Core/build.gradle.kts
@@ -64,32 +64,6 @@ tasks.test {
 	useJUnitPlatform()
 }
 
-tasks.jar {
-	// Fuck Java 9
-	exclude("**/module-info.class")
-
-	exclude("META-INF/LICENSE.txt")
-	exclude("META-INF/LICENSE")
-	exclude("META-INF/NOTICE.txt")
-	exclude("META-INF/NOTICE")
-
-	// Fuck Netty !!!!!!
-	exclude("META-INF/INDEX.LIST")
-	exclude("META-INF/*.properties")
-	exclude("META-INF/*/*.properties")
-	exclude("META-INF/*/*/*.properties")
-	exclude("META-INF/*/*/*/*.properties")
-	exclude("META-INF/*.xml")
-	exclude("META-INF/*/*.xml")
-	exclude("META-INF/*/*/*.xml")
-	exclude("META-INF/*/*/*/*.xml")
-
-	manifest {
-		 attributes(mapOf("Main-Class" to "cn.rwhps.server.Main"))
-		 attributes(mapOf("Launcher-Agent-Class" to  "cn.rwhps.server.dependent.LibraryManager"))
-	}
-}
-
 publishing {
 	publications {
 		create<MavenPublication>("maven") {

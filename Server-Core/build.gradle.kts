9090a76b24fb6dd0e63feb740e3effe7223ccfd2
deng-rui <dr@der.kim>
2022-06-04 15:11:04 +0800

    test

--- Server-Core/build.gradle.kts
+++ Server-Core/build.gradle.kts
@@ -64,46 +64,49 @@ tasks.test {
 	useJUnitPlatform()
 }
 
-publishing {
-	publications {
-		create<MavenPublication>("maven") {
-			groupId = "com.github.RW-HPS"
-			artifactId = "Server-Core"
-			description = "Dedicated to Rusted Warfare(RustedWarfare) High Performance Server"
-			version = "1.0.0"
+tasks.register("testBoth") {
+	doLast {
+		println("buildFinished()->构建结束")
+		publishing {
+			publications {
+				create<MavenPublication>("maven") {
+					groupId = "com.github.RW-HPS"
+					artifactId = "Server-Core"
+					description = "Dedicated to Rusted Warfare(RustedWarfare) High Performance Server"
+					version = "1.0.0"
 
-			//from (components.getByName("java"))
-			//from (components.getByName("kotlin"))
-			artifact(File("build/libs/").listFiles()[0])
+					from(components.getByName("java"))
 
-			pom {
-				scm {
-					url.set("https://github.com/RW-HPS/RW-HPS")
-					connection.set("scm:https://github.com/RW-HPS/RW-HPS.git")
-					developerConnection.set("scm:git@github.com:RW-HPS/RW-HPS.git")
-				}
+					pom {
+						scm {
+							url.set("https://github.com/RW-HPS/RW-HPS")
+							connection.set("scm:https://github.com/RW-HPS/RW-HPS.git")
+							developerConnection.set("scm:git@github.com:RW-HPS/RW-HPS.git")
+						}
 
-				licenses {
-					license {
-						name.set("GNU AGPLv3")
-						url.set("https://github.com/RW-HPS/RW-HPS/blob/master/LICENSE")
+						licenses {
+							license {
+								name.set("GNU AGPLv3")
+								url.set("https://github.com/RW-HPS/RW-HPS/blob/master/LICENSE")
+							}
+						}
+
+						developers {
+							developer {
+								id.set("RW-HPS")
+								name.set("RW-HPS Technologies")
+							}
+						}
+
+					}
+
+					pom.withXml {
+						val root = asNode()
+						root.appendNode("description", project.description)
+						root.appendNode("name", project.name)
+						root.appendNode("url", "https://github.com/RW-HPS/RW-HPS")
 					}
 				}
-
-				developers {
-					developer {
-						id.set("RW-HPS")
-						name.set("RW-HPS Technologies")
-					}
-				}
-
-			}
-
-			pom.withXml {
-				val root = asNode()
-				root.appendNode("description", project.description)
-				root.appendNode("name", project.name)
-				root.appendNode("url", "https://github.com/RW-HPS/RW-HPS")
 			}
 		}
 	}

26478e72299966e439c80e647ba4ce44bf7d05e5
deng-rui <dr@der.kim>
2022-06-04 15:35:47 +0800

    Failure

--- Server-Core/build.gradle.kts
+++ Server-Core/build.gradle.kts
@@ -64,60 +64,45 @@ tasks.test {
 	useJUnitPlatform()
 }
 
-gradle.addBuildListener(object : BuildListener {
-	override fun settingsEvaluated(settings: Settings) {
-	}
+publishing {
+	publications {
+		create<MavenPublication>("maven") {
+			groupId = "com.github.RW-HPS"
+			artifactId = "Server-Core"
+			description = "Dedicated to Rusted Warfare(RustedWarfare) High Performance Server"
+			version = "1.0.0"
 
-	override fun buildFinished(result: BuildResult) {
-		println("buildStarted()->构建结束")
-		publishing {
-			publications {
-				create<MavenPublication>("maven") {
-					groupId = "com.github.RW-HPS"
-					artifactId = "Server-Core"
-					description = "Dedicated to Rusted Warfare(RustedWarfare) High Performance Server"
-					version = "1.0.0"
+			from(components.getByName("java"))
 
-					from(components.getByName("java"))
+			pom {
+				scm {
+					url.set("https://github.com/RW-HPS/RW-HPS")
+					connection.set("scm:https://github.com/RW-HPS/RW-HPS.git")
+					developerConnection.set("scm:git@github.com:RW-HPS/RW-HPS.git")
+				}
 
-					pom {
-						scm {
-							url.set("https://github.com/RW-HPS/RW-HPS")
-							connection.set("scm:https://github.com/RW-HPS/RW-HPS.git")
-							developerConnection.set("scm:git@github.com:RW-HPS/RW-HPS.git")
-						}
-
-						licenses {
-							license {
-								name.set("GNU AGPLv3")
-								url.set("https://github.com/RW-HPS/RW-HPS/blob/master/LICENSE")
-							}
-						}
-
-						developers {
-							developer {
-								id.set("RW-HPS")
-								name.set("RW-HPS Technologies")
-							}
-						}
-
-					}
-
-					pom.withXml {
-						val root = asNode()
-						root.appendNode("description", project.description)
-						root.appendNode("name", project.name)
-						root.appendNode("url", "https://github.com/RW-HPS/RW-HPS")
+				licenses {
+					license {
+						name.set("GNU AGPLv3")
+						url.set("https://github.com/RW-HPS/RW-HPS/blob/master/LICENSE")
 					}
 				}
+
+				developers {
+					developer {
+						id.set("RW-HPS")
+						name.set("RW-HPS Technologies")
+					}
+				}
+
+			}
+
+			pom.withXml {
+				val root = asNode()
+				root.appendNode("description", project.description)
+				root.appendNode("name", project.name)
+				root.appendNode("url", "https://github.com/RW-HPS/RW-HPS")
 			}
 		}
 	}
-
-	override fun projectsEvaluated(gradle: Gradle) {
-	}
-
-	override fun projectsLoaded(gradle: Gradle) {
-
-	}
-})
+}

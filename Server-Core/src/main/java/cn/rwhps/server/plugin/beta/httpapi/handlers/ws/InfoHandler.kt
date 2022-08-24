87bdc88bddea4290980cbfed476363577c110053
zhou2008 <493505110@qq.com>
2022-08-24 22:39:13 +0800

    TODO

---
+++ Server-Core/src/main/java/cn/rwhps/server/plugin/beta/httpapi/handlers/ws/InfoHandler.kt
@@ -0,0 +1,10 @@
+package cn.rwhps.server.plugin.beta.httpapi.handlers.ws
+
+import cn.rwhps.server.plugin.beta.httpapi.handlers.BaseWsHandler
+import io.netty.channel.Channel
+
+class InfoHandler : BaseWsHandler() {
+    override fun run(msg: String, channel: Channel) {
+        TODO("Not yet implemented")
+    }
+}

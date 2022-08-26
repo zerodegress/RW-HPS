303042a404b84fb14c992f988bffc238df41229b
deng-rui <dr@der.kim>
2022-08-26 19:47:27 +0800

    6.4.0

--- Server-Core/src/main/java/cn/rwhps/server/data/global/Data.kt
+++ Server-Core/src/main/java/cn/rwhps/server/data/global/Data.kt
@@ -44,7 +44,10 @@ object Data {
     /** 自定义包名  */
     const val SERVER_ID = "cn.rwhps.server"
     const val SERVER_ID_RELAY = "cn.rwhps.server.relayCustomMode.Dr"
-    const val SERVER_CORE_VERSION = "6.0.0"
+    const val SERVER_ID_RELAY_GET = "cn.rwhps.server.relayGetUUIDHex.Dr"
+    const val SERVER_CORE_VERSION = "6.4.0"
+    const val TOPT_KEY = "cn.rwhps.server.topt # RW-HPS Team"
+    const val SERVER_RELAY_UUID = "RCN Team & Tiexiu.xyz Core Team"
     /** 单位数据缓存  */
 	@JvmField val utilData = CompressOutputStream.getGzipOutputStream("customUnits", false)
 

2379e7513ce9ba52a57e2fd342fb05b1f546492d
deng-rui <dr@der.kim>
2022-08-26 19:12:16 +0800

    Add Maven dependencies for easy use of the Lib Manager Remove some unused dependencies Update some vulnerable dependencies

--- Server-Core/build.gradle.kts
+++ Server-Core/build.gradle.kts
@@ -10,9 +10,11 @@ val nettyVersion = "4.1.79.Final"
  * Fuck implementation
  */
 dependencies {
-	api("org.jetbrains.kotlin:kotlin-stdlib:1.6.20")
+	api("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
 
+	//implementation(project(":RUDP"))
 	implementation(project(":TimeTaskQuartz"))
+	// 并没有使用 AIO
 	//api "cn.hutool:hutool-socket:5.7.5"
 
 	api("io.netty:netty-buffer:$nettyVersion")
@@ -23,49 +25,39 @@ dependencies {
 	api("io.netty:netty-transport-native-epoll:$nettyVersion:linux-aarch_64")
 	api("io.netty:netty-transport-native-epoll:$nettyVersion:linux-x86_64")
 
-//	api (group: "io.netty", name: "netty-all", version: "4.1.73.val") {
-//		exclude group: "io.netty", module: "netty-resolver"
-//	}
-
-	//api fileTree(dir:"libs",include:["ChainMarket-23fc7f989f.jar"])
-
 	api("com.github.deng-rui:RUDP:2.0.0")
-	//api("com.github.deng-rui:Compress-Zip:1.0.0")
 
+	// Json 解析
+	// 我建议使用 RW-HPS Json 方法 而不是直接使用依赖
 	api("com.google.code.gson:gson:2.9.0")
+	api("org.json:json:20220320")
 
 	api("org.apache.commons:commons-compress:1.21")
-	api("org.apache.commons:commons-text:1.9")
 
-
-	//compileOnly group: "com.ip2location", name: "ip2location-java", version: "8.5.0"
-	//compileOnly group: "com.alibaba", name: "fastjson", version: "1.2.58"
 	api("com.squareup.okhttp3:okhttp:4.10.0") {
 		exclude(group = "org.jetbrains.kotlin")
 	}
-	api("com.vdurmont:emoji-java:5.1.1")
+	api("com.vdurmont:emoji-java:5.1.1") {
+		exclude(group = "org.json")
+	}
 
-//	implementation("org.apache.maven:maven-core:3.8.6")
+
+	implementation("org.apache.maven:maven-core:3.8.6")
+	implementation("org.lionsoul:ip2region:1.7.2")
 	implementation("org.jline:jline-reader:3.21.0")
 	implementation("org.fusesource.jansi:jansi:2.4.0")
 
 
 	compileOnly("commons-io:commons-io:2.11.0")
 	compileOnly("it.unimi.dsi:fastutil:8.5.8")
-	//compileOnly group: "org.bouncycastle", name: "bcprov-jdk15on", version: "1.69"
-
-	//compileOnly fileTree(dir:"libs",include:["*.jar"])
-	//compileOnly group: "org.quartz-scheduler", name: "quartz", version: "2.3.2"
-	//compileOnly group: "com.github.oshi", name: "oshi-core", version: "5.5.0"
 
 	testApi("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-
-
 }
 
 tasks.test {
 	useJUnitPlatform()
 }
+
 
 publishing {
 	publications {
@@ -73,6 +65,7 @@ publishing {
 			groupId = "com.github.RW-HPS"
 			artifactId = "Server-Core"
 			description = "Dedicated to Rusted Warfare(RustedWarfare) High Performance Server"
+			version = "1.0.0"
 
 			from(components.getByName("java"))
 

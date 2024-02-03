46c0c453caf4815238ec675ccb88012c054b4f03
deng-rui <dr@der.kim>
2024-02-03 19:59:15 +0800

    Updata Lib Version
    Signed-off-by: deng-rui <dr@der.kim>

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2023 RW-HPS Team and contributors.
+ * Copyright 2020-2024 RW-HPS Team and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
@@ -13,10 +13,12 @@
  */
 object Versions {
     // Netty Version
-    const val nettyVersion = "4.1.101.Final"
+    const val nettyVersion = "4.1.106.Final"
+
+    const val jLineVersion = "3.25.1"
 
     // Kotlin Version
-    const val kotlinVersion = "1.9.10"
+    const val kotlinVersion = "1.9.20"
 
     // Graalvm Version
     const val graalvmVersion = "22.3.2"
